db843bf9d874af9bef35a03e84c68957ed12b243
Sourav Chatterjee <schatterjee.0404@gmail.com>
2023-08-28 14:53:45 +0530

    second commit

--- src/Pages/BlogDetails.js
+++ src/Pages/BlogDetails.js
@@ -1,37 +1,3 @@
-// import React from 'react';
-// import { useParams } from 'react-router-dom';
-
-// const BlogDetails = ({ posts }) => {
-//     const { id } = useParams();
-//     const postId = parseInt(id);
-//     const post = posts.find((post) => post.id === postId);
-
-//     if (!post) {
-//         return <div>Post not found</div>;
-//     }
-
-//     if (!posts || posts.length === 0) {
-//         return <div>No posts available</div>;
-//     }
-
-//     return (
-//         <>
-//             <div className="container mt-2">
-             
-//                 {post.title1 && <h1>{post.title1}</h1>}
-//                 <hr />
-//                 {post.content1 && <p>{post.content1}</p>}
-
-//                 {post.title2 && <h3>{post.title2}</h3>}
-//                 <hr />
-//                 {post.content2 && <p>{post.content2}</p>}
-//             </div>
-//         </>
-//     );
-// };
-
-// export default BlogDetails;
-
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import 'github-markdown-css'; // Optional, for styling the Markdown content
